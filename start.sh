a7952bcc763c31db1ee1f018fb89bce3022ecf81
kevin <kevinz@weghst.com>
2017-12-26 19:08:20 +0800

    增加启动脚本

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# This file should be used to prepare and run your WebProxy after set up your .env file
+# Source: https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion
+#
+
+# 1. Check if .env file exists
+if [ -e .env ]; then
+    source .env
+else 
+    echo "Please set up your .env file before starting your enviornment."
+    exit 1
+fi
+
+# 2. Create docker network
+docker network create $NETWORK
+
+# 3. Update local images
+docker-compose pull
+
+# 4. Start proxy
+
+docker-compose up -d
+
+exit 0
